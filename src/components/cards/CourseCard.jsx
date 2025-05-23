64149c8d80f2d84b88543ff882f17b6f7ad0aaba
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2025-05-23 13:04:07 +0200

    feat: enhance CourseCard to fetch course image dynamically; fallback to default image on error or absence of image

--- src/components/cards/CourseCard.jsx
+++ src/components/cards/CourseCard.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import defaultCourseImg from '../../assets/default-course.svg'; // Importa la imagen predeterminada
-
+import React from "react";
+import { useEffect, useState } from "react";
+import defaultCourseImg from "../../assets/default-course.svg"; // Importa la imagen predeterminada
+import MediaRepository from "../../api/media/MediaRepository";
 export default function CourseCard({ course }) {
-  const courseImage = course.photo.url === null ? defaultCourseImg: course.photo.url; // Usa la imagen del curso o la predeterminada
+    const [courseImage, setCourseImage] = useState(defaultCourseImg);
 
+    useEffect(() => {
+      const fetchCourseImage = async () => {
+        try {
+          let image = await MediaRepository.getUrlSingleObject('courses', course.id, 'photo');
+          if (!image || image.url === null) {
+            setCourseImage(defaultCourseImg); // Usa la imagen predeterminada si no hay imagen
+          } else {
+            setCourseImage(image.url);
+          }
+        } catch (error) {
+          console.error("Error fetching course image:", error);
+          setCourseImage(defaultCourseImg); // Usa la imagen predeterminada en caso de error
+        }
+      };
+  
+      fetchCourseImage();
+    }, [course.id]);
   return (
     <div className="course-card">
       <img src={courseImage} alt={course.name} className="course-image" />
       <h3 className="course-title">{course.name}</h3>
       <p className="course-description">{course.description}</p>
       <p className="course-price">
-        {course.is_free ? 'Gratis' : `$${course.price}`}
+        {course.is_free ? "Gratis" : `$${course.price}`}
       </p>
       <button className="btn btn-secondary">Ver Curso</button>
     </div>
