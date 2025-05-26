1c263d08f98718f39c6d42fe9d6305217d26ed1b
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2025-05-27 01:35:34 +0200

    feat: enhance CourseCard component to display availability status; update button text and price visibility based on course availability

--- src/components/cards/CourseCard.jsx
+++ src/components/cards/CourseCard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import defaultCourseImg from "../../assets/default-course.svg"; // Importa la imagen predeterminada
 import { Link } from "react-router-dom";
+import { useAvailableContentStore } from "../../stores/useAvailableContentStore";
 export default function CourseCard({ course }) {
   // console.log("CourseCard", course);
+  const {availableCoursesIds} = useAvailableContentStore();
+
+  const additionalClasses = availableCoursesIds.includes(course.id) ? "available" : "unavailable";
 
   return (
-    <div className="course-card">
+    <div className={`course-card ${additionalClasses}`}>
       <img
         src={course.photo || defaultCourseImg}
         alt={course.name}
@@ -15,11 +19,15 @@ export default function CourseCard({ course }) {
       <p className="course-description">
         {course.description} | {course.estimated_hours} hours
       </p>
-      <p className="course-price">
-        {course.is_free ? "Gratis" : `$${course.price}`}
-      </p>
+      {!availableCoursesIds.includes(course.id) && (
+        <p className="course-price">
+          {course.is_free ? "Gratis" : `$${course.price}`}
+        </p>
+      )}
       <Link to={'/courses/' + course.id} className="course-link">
-        <button className="btn btn-secondary">Ver Curso</button>
+      <button className={`btn ${availableCoursesIds.includes(course.id) ? "btn-primary" : "btn-secondary"}`}>
+        {availableCoursesIds.includes(course.id) ? "Ver Curso" : "Inscribirse"}
+      </button>
       </Link>
     </div>
   );
